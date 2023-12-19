2c7623ce473bcb7c966552520d6cb9130478e569
Mashood <154373912+Mashoodkhann@users.noreply.github.com>
2023-12-19 22:31:19 +0000

    initial commit

---
+++ pythonProject2/Bonus1.py
@@ -0,0 +1,5 @@
+text = input ('Enter a title')
+
+legnth = len(text)
+
+print (legnth)
---
+++ pythonProject2/main.py
@@ -0,0 +1,10 @@
+user_prompt = ('enter a todo')
+
+while 2>1:
+    todo = input(user_prompt)
+    print('after a hour')
+    input(user_prompt)
+    print('after two hours')
+    input(user_prompt)
+    print('after 3 hours')
+    print ('in total it takes 5 hrs')
